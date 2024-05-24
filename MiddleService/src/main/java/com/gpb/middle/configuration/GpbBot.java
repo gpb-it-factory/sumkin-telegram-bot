154bcf3f757f006821523d4e239bce6898b4d180
Petr Sumkin <sumkin.gpb@gmail.com>
2024-05-24 15:26:25 +0300

    Задание 3. Создан «скелет» бота (#3)
    * Добавлен скелет бота, первичная архитектура и обработка ping
    
    * Добавлен скелет бота, первичная архитектура и обработка ping
    
    * Исправлен application.yaml (вместо properties), перенесена логика обработки команд в сервисный слой.
    
    * Исправлен application.yaml (вместо properties), перенесена логика обработки команд в сервисный слой.

---
+++ MiddleService/src/main/java/com/gpb/middle/configuration/GpbBot.java
@@ -0,0 +1,55 @@
+package com.gpb.middle.configuration;
+
+import com.gpb.middle.service.CommandsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.longpolling.BotSession;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.AfterBotRegistration;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+
+@Service
+@Slf4j
+@PropertySource("classpath:secret.keys")
+public class GpbBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
+
+    private static final String PING = "/ping";
+
+    private final String token;
+    private final CommandsService commandsService;
+
+    public GpbBot(@Value("${token}") String token, CommandsService commandsService) {
+        this.token = token;
+        this.commandsService = commandsService;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer() {
+        return this;
+    }
+
+    @Override
+    public void consume(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message_text = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            commandsService.switchService(message_text, chatId);
+        }
+    }
+
+    @AfterBotRegistration
+    public void afterRegistration(BotSession botSession) {
+        log.info("Бот запущен, статус: " + botSession.isRunning());
+    }
+
+}
---
+++ MiddleService/src/main/java/com/gpb/middle/service/CommandsService.java
@@ -0,0 +1,48 @@
+package com.gpb.middle.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+@Service
+@Slf4j
+@PropertySource("classpath:secret.keys")
+public class CommandsService {
+
+    private static final String PING = "/ping";
+    private final TelegramClient telegramClient;
+
+    public CommandsService(@Value("${token}") String token)  {
+        this.telegramClient = new OkHttpTelegramClient(token);
+    }
+
+    public void switchService(String message_text, long chatId) {
+        if (message_text.startsWith("/")) {
+            switch (message_text) {
+                case PING -> sendReply(chatId, "pong");
+                default -> sendReply(chatId,
+                        "Мне пока что нечего на это ответить");
+            }
+        }
+    }
+
+    private void sendReply(long chatId, String message_text) {
+
+        SendMessage message = SendMessage
+                .builder()
+                .chatId(chatId)
+                .text(message_text)
+                .build();
+        try {
+            telegramClient.execute(message);
+            log.info("Отправил сообщение \"{}\" получателю {}", message_text, chatId);
+        } catch (TelegramApiException e) {
+            log.error("Ошибка отправки сообщения \"{}\" получателю {}", message_text, chatId);
+        }
+    }
+}
